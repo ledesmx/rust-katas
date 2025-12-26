30f60d459faf83d46be439dc9d9054ffec6be50d
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2025-12-25 18:55:35 -0600

    add intercambio de variables

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    // 1. Intercambio sin variables temporales
+    let a = inter((3, 5));
+    println!("{} {}", a.0, a.1);
+
+    let a = inter2((3, 5.99, 'a', true));
+    println!("{} {} {} {}", a.0, a.1, a.2, a.3);
+}
+
+// 1. Intercambio sin variables temporales
+
+// Dada una tupla (i32, i32) que representa (a, b), escribe un programa que intercambie los valores y devuelva una nueva tupla (b, a).
+
+// 👉 Restricción:
+
+// No puedes usar variables temporales adicionales (solo destructuring).
+
+// Pregunta clave:
+// ¿Cómo puedes usar el destructuring para “reordenar” valores?
+
+fn inter(x: (i32, i32)) -> (i32, i32) {
+    let (n1, n2) = x;
+    (n2, n1)
+}
+fn inter2(nums: (i32, f64, char, bool)) -> (bool, char, i32, f64) {
+    (nums.3, nums.2, nums.0, nums.1)
 }
