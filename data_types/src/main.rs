a857bda838700facd856b854ccf40e6f63d2645e
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2025-12-25 19:09:23 -0600

    agregar analizador de punto en el plano

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -5,6 +5,13 @@ fn main() {
 
     let a = inter2((3, 5.99, 'a', true));
     println!("{} {} {} {}", a.0, a.1, a.2, a.3);
+
+    // 2. Analizador de punto en el plano
+    println!("{}", which_plane((3, -5)));
+    println!("{}", which_plane((0, 0)));
+    println!("{}", which_plane((3, 5)));
+    println!("{}", which_plane((-4, -5)));
+    println!("{}", which_plane((0, -5)));
 }
 
 // 1. Intercambio sin variables temporales
@@ -24,4 +31,57 @@ fn inter(x: (i32, i32)) -> (i32, i32) {
 }
 fn inter2(nums: (i32, f64, char, bool)) -> (bool, char, i32, f64) {
     (nums.3, nums.2, nums.0, nums.1)
+}
+
+// 2. Analizador de punto en el plano
+
+// Representa un punto en 2D como una tupla (i32, i32).
+
+// Escribe un programa que:
+
+// Determine si el punto está:
+
+// en el origen
+
+// sobre el eje X
+
+// sobre el eje Y
+
+// o en un cuadrante
+
+// 👉 Usa match sobre la tupla.
+
+// Pregunta clave:
+// ¿Cómo puedes usar patrones como (0, y) o (x, 0)?
+fn which_plane(vector: (i32, i32)) -> String {
+    let (x, y) = vector;
+
+    let result: String;
+
+    if x > 0 {
+        if y > 0 {
+            result = String::from("right up quadrant");
+        } else if y < 0 {
+            result = String::from("right down quadrant");
+        } else {
+            result = String::from("on axis x right");
+        }
+    } else if x < 0 {
+        if y > 0 {
+            result = String::from("left up quadrant");
+        } else if y < 0 {
+            result = String::from("left down quadrant");
+        } else {
+            result = String::from("on axis x left");
+        }
+    } else {
+        if y > 0 {
+            result = String::from("on axis y up");
+        } else if y < 0 {
+            result = String::from("on axis y down");
+        } else {
+            result = String::from("center");
+        }
+    }
+    result
 }
