1cb71fa04435b94afde06e75a2ff8446da9403e5
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2026-01-07 09:27:46 -0600

    agregar procesar mensaje

--- what_is_ownership/src/main.rs
+++ what_is_ownership/src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
+    // 🧩 PARTE 1 — Ejercicios iniciales (entender ownership)
     entender_ownership();
+
+    // 🔥 RETO — Procesar un mensaje correctamente
+    let mensaje = crear_prioridad(6);
+    let mensaje = agregar_tipo(String::from("LOG"), mensaje);
+    let mensaje = agregar_mensaje(String::from(""), mensaje);
+    println!("{}", mensaje);
 }
 
 // 🧩 PARTE 1 — Ejercicios iniciales (entender ownership)
@@ -32,4 +39,83 @@ fn entender_ownership() {
     }
     // println!("{z}"); // z out of scope
     // println!("{s}"); // borrow of moved
+}
+
+// 🔥 RETO — Procesar un mensaje correctamente
+// 🧩 Problema
+
+// Un programa recibe un mensaje de texto y debe usarlo una sola vez.
+
+// El mensaje:
+
+// se crea
+
+// se valida
+
+// se muestra
+
+// Después de mostrarse, no debe poder volver a usarse.
+
+// ✅ Qué debes validar
+
+// El mensaje es válido si tiene más de 5 caracteres.
+
+// 📌 Qué debes hacer
+
+// Crear el mensaje (String)
+
+// Pasarlo a una función que lo valide
+
+// Si es válido, pasarlo a otra función que lo muestre
+
+// Si no es válido, terminar el programa
+
+// 📜 Reglas
+
+// Cada paso es una función
+
+// No copies ni clones el mensaje
+
+// El mensaje debe moverse entre funciones
+
+// El compilador debe impedir usos incorrectos
+
+// 🎯 Objetivo
+
+// Usar ownership para evitar:
+
+// usar mensajes sin validar
+
+// usar mensajes más de una vez
+
+// 🤔 Pregunta clave
+
+// ¿Por qué después de mostrar el mensaje ya no puede existir?
+
+fn crear_prioridad(prioridad: i32) -> String {
+    let mensaje = if prioridad > 0 && prioridad < 10 {
+        format!("{} - ", prioridad)
+    } else {
+        String::from("N - ")
+    };
+    mensaje
+}
+
+fn agregar_tipo(tipo: String, prev: String) -> String {
+    let mensaje = if tipo == "TIP" || tipo == "LOG" {
+        prev + &tipo
+    } else {
+        prev + "000"
+    };
+    mensaje
+}
+fn agregar_mensaje(men: String, prev: String) -> String {
+    let mensaje: String = if men.is_empty() {
+        prev + " - ~No Message~"
+    } else if men.len() > 11 {
+        prev + " - ~Too long..~"
+    } else {
+        prev + " - " + &men
+    };
+    mensaje
 }
