9787b7aec3ea34378f4a2368a16ab3acbd7d548a
ledesmx <alberto.ledesma.dev@gmail.com>
2026-01-30 23:02:58 -0600

    add last word

--- the_slice_type/src/main.rs
+++ the_slice_type/src/main.rs
@@ -2,9 +2,13 @@ fn main() {
     let text1 = String::from("Bad Bunny");
     let text2 = &String::from("Losing my religion");
     let text3 = "Can you read my mind?";
-    println!("{}", first_word(&text1));
-    println!("{}", first_word(text2));
-    println!("{}", first_word(text3));
+    println!("first {}", first_word(&text1));
+    println!("first {}", first_word(text2));
+    println!("first {}", first_word(text3));
+
+    println!("last {}", last_word(&text1));
+    println!("last {}", last_word(&text2));
+    println!("last {}", last_word(&text3));
 }
 // 🟢 Ejercicio 1 — Primera palabra (slice básico)
 
@@ -29,6 +33,38 @@ fn first_word(s: &str) -> &str {
         if c == ' ' {
             return &s[..i];
         }    
+    }
+    s
+}
+
+// 🟢 Ejercicio 2 — Última palabra
+
+// Objetivo: Pensar slices desde el final.
+
+// Enunciado
+
+// Devuelve la última palabra de una cadena.
+
+// Firma
+// fn last_word(s: &str) -> &str
+
+// Ejemplo
+// let s = "rust es genial";
+// assert_eq!(last_word(s), "genial");
+
+// Restricciones
+
+// Recorre los bytes
+
+// Usa slices, no split
+
+// 👉 Aquí practicas rangos tipo [i+1..].
+fn last_word(s: &str) -> &str {
+    for (i, c) in s.char_indices().rev() {
+        if c == ' ' {
+            return &s[i+1..];
+        }
+        // println!("{} {}", i, c);
     }
     s
 }
