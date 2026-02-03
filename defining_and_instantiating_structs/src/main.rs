9614b97e4e5929fd4d51f8ba8f36c7fb5014bc05
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2026-02-03 12:04:04 -0600

    add login count

--- defining_and_instantiating_structs/src/main.rs
+++ defining_and_instantiating_structs/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     let u1 = create_user(String::from("zymmer"), String::from("zym@gmail.com"));
     println!("{} {} {} {}", u1.username, u1.email, u1.active, u1.sign_in_count);
+
+    let mut u1 = u1;
+    login(&mut u1);
+    login(&mut u1);
+    println!("{} {} {} {}", u1.username, u1.email, u1.active, u1.sign_in_count);
 }
 
 // Ejercicio 1 - Constructor seguro de usuario
@@ -23,4 +28,11 @@ fn create_user(username: String, email: String) -> User {
         active: true,
         sign_in_count: 1,
     }
+}
+
+// Ejercicio 2 - Contador de inicios de sesion
+// Objetivo: Practicar mutabilidad en structs, paso por referencioa mutable
+// Escribe una funcion que incremente el contador de inicio de sesion de un usuario, usa  &mut User
+fn login(user: &mut User) {
+    user.sign_in_count = user.sign_in_count + 1;
 }
