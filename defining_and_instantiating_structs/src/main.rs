f2cbcf039fc304741234bffde9be276cfb4ae91a
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2026-02-04 09:22:17 -0600

    agregar change email

--- defining_and_instantiating_structs/src/main.rs
+++ defining_and_instantiating_structs/src/main.rs
@@ -6,6 +6,9 @@ fn main() {
     login(&mut u1);
     login(&mut u1);
     println!("{} {} {} {}", u1.username, u1.email, u1.active, u1.sign_in_count);
+
+    let u1 = change_email(u1, String::from("zynzyn@gmail.com"));
+    println!("{} {}", u1.username, u1.email);
 }
 
 // Ejercicio 1 - Constructor seguro de usuario
@@ -35,4 +38,14 @@ fn create_user(username: String, email: String) -> User {
 // Escribe una funcion que incremente el contador de inicio de sesion de un usuario, usa  &mut User
 fn login(user: &mut User) {
     user.sign_in_count = user.sign_in_count + 1;
+}
+
+// Ejercicio 3 - Clon parcial con struct update syntax
+// Objetivo: Entender que se mueve y que no en structs
+// Dado un usuario existente, crea uno nuevo que tenga el mismo username, active y sign_in_count. Pero otro email.
+fn change_email(user: User, new_email: String) -> User {
+    User{
+        email: new_email,
+        ..user
+    }
 }
